2c933c385ab56711d496101831c6b57b4eaae964
cheb4 <a.splitas@gmail.com>
2019-07-31 20:19:06 +0300

    first commit

---
+++ stuffClass.js
@@ -0,0 +1,87 @@
+class Pomodoro {
+  constructor(_time) {
+    this.time = _time;
+  }
+  get get_time() {
+    return this.time;
+  }
+  set set_time(new_time) {
+    this.time = new_time;
+  }
+
+  // jump 1   
+  countdown(countDownDate) {
+    // jump 2   p
+    this.intervalID = setInterval(function(countTo = countDownDate) {
+      // jump 3   a
+      // Get today's date and time
+      let now = new Date().getTime();
+
+      // Find the distance between now and the count down date
+
+      this.distance = countTo - now;
+      console.log("distance", distance);
+      // Time calculations for days, hours, minutes and seconds
+      let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      // Output the result in an element with id="clockH1"document.querySelector("#clockH1")
+      document.querySelector("#clockH1").innerHTML =
+        minutes + "m " + seconds + "s ";
+
+      // If the count down is over, write some text
+      if (distance < 0) {
+        clearInterval(this.intervalID);
+        document.querySelector("#clockH1").innerHTML = "get rest";
+      }
+    }, 1000);
+  }
+  get distanceGetter() {
+    return this.distance;
+  }
+  killCountdown() {
+    console.log(`killed countdown (${this.intervalID})`);
+    // console.log(`killed at ${this.distance}`);
+    console.log(this.shit);
+    clearInterval(this.intervalID);
+  }
+}
+
+let countDownDate = new Date(Date.parse(new Date()) + 25 * 60 * 1000);
+// main object
+let pom = new Pomodoro(countDownDate);
+pom.countdown(countDownDate);
+// pom.opa();
+console.log(pom.get_time);
+
+// reset
+
+document.querySelector("#reset").onclick = () => {
+  pom.killCountdown();
+  pom.countdown(new Date(Date.parse(new Date()) + 25 * 60 * 1000));
+};
+
+// stop
+document.querySelector("#stop").onclick = () => {
+  pom.killCountdown();
+};
+// start
+document.querySelector("#start").onclick = () => {
+  console.log("opa");
+  currentTimeOnScreen = document.querySelector("#clockH1").textContent;
+  // regex
+  regex = /\d\d?/gi;
+  timeArray = currentTimeOnScreen.match(regex);
+  // regex
+  pom.killCountdown();
+  console.log(timeArray);
+  let newCountDate = new Date(
+    Date.parse(new Date()) + timeArray[0] * timeArray[1] * 1000
+  );
+  console.log("from start", newCountDate);
+  pom.countdown(newCountDate);
+};
+// document.querySelector("#clockH1");
+// pom.set_time = 9;
+// console.log(pom.get_time);
+// console.log(countDownDate);
